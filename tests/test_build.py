03d2af1cd59fe728f3bd4183dd4d35ce70c7db23
Kieran Geary <kieran.r.geary@gmail.com>
2025-04-14 17:40:09 +0100

    Update tests for windows compatibility

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
 import re
+import os
 import sys
+import time
+import random
 import zipfile
 from pathlib import Path
 from subprocess import run, PIPE
@@ -19,10 +22,11 @@ def test_pex_build(new_project: Path, proc_stdout: str) -> None:
     assert dist.is_dir()
     binaries = list(dist.iterdir())
     assert len(binaries) == 1
-    assert binaries[0].suffix == ".pex"
-    assert zipfile.is_zipfile(binaries[0])
+    prog = binaries[0]
+    assert prog.suffix == ".pex"
+    assert zipfile.is_zipfile(prog)
 
-    proc = run(binaries, stdout=PIPE, universal_newlines=True)
+    proc = run([sys.executable, prog], stdout=PIPE, universal_newlines=True)
     assert proc.returncode == 0
     assert proc.stdout == proc_stdout
 
@@ -43,6 +47,9 @@ def test_scie_build(new_project: Path, proc_stdout: str) -> None:
 scie = 'eager'
 """)
 
+    # GitHub actions rate-limit workaround
+    if os.getenv("GITHUB_ACTIONS"):
+        time.sleep(random.randint(0, 10))
     proc = run([sys.executable, "-m", "hatch", "build", "-t", "pex"])
     proc.check_returncode()
 
@@ -55,7 +62,7 @@ scie = 'eager'
     assert zipfile.is_zipfile(artifacts[1])
 
     for binary in artifacts:
-        proc = run([binary], stdout=PIPE, universal_newlines=True)
+        proc = run([sys.executable, binary], stdout=PIPE, universal_newlines=True)
         assert proc.returncode == 0
         assert proc.stdout == proc_stdout
 
@@ -85,9 +92,10 @@ def test_interactive_pex(new_project: Path) -> None:
     artifacts = list(dist.iterdir())
     artifacts.sort()
     assert len(artifacts) == 1
-    assert artifacts[0].suffix == ".pex"
-    assert zipfile.is_zipfile(artifacts[0])
+    prog = artifacts[0]
+    assert prog.suffix == ".pex"
+    assert zipfile.is_zipfile(prog)
 
-    proc = run(artifacts + ["-V"], stdout=PIPE, universal_newlines=True)
+    proc = run([sys.executable, prog, "-V"], stdout=PIPE, universal_newlines=True)
     proc.check_returncode()
     assert "Python" in proc.stdout
